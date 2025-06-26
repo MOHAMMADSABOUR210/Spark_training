4c9a76c855445e03855d8a4e223f77015e3d7e37
MOHAMMAD SABOUR <m210saboor@gmail.com>
2025-06-26 18:31:29 +0330

    make something new for learning filter

--- test_pyspark.py
+++ test_pyspark.py
@@ -5,16 +5,25 @@ os.environ["PYSPARK_DRIVER_PYTHON"] = r"C:\Program Files\Python310\python.exe"
 from pyspark import SparkContext
 
 sc = SparkContext(master="local", appName="MyApp")
+
 lines = sc.textFile(r"C:\spark\README.md")
 
 
 print(lines.count())
 print(lines.first())
 
+
+
+print("way One : Use functions")
 def HasPython(line):
     return "Python" in line
-
 Pythonlines = lines.filter(lambda line : HasPython(line))
 print(Pythonlines.count())
 print(Pythonlines.first())
+
+print("way two : in line functions")
+Pythonlines = lines.filter(lambda line : "Python" in line)
+print(Pythonlines.count())
+print(Pythonlines.first())
+
 
